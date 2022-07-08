4b76efb405f6576e8b2526d01a044d4aa605f6c7
Minyong Oh <dhalsdyd@gmail.com>
2022-07-08 22:55:09 +0900

    Add : Button Widget style

---
+++ lib/app/widgets/button.dart
@@ -0,0 +1,98 @@
+import 'package:firebase_getx_boilerplate/app/core/color_theme.dart';
+import 'package:firebase_getx_boilerplate/app/core/text_theme.dart';
+import 'package:flutter/material.dart';
+
+//Firbase_Getx_BoilerPlate...!
+class FGBPButton extends StatelessWidget {
+  const FGBPButton({
+    Key? key,
+    required this.child,
+    this.color = AppColorTheme.mainColor,
+    this.height,
+    this.width,
+    this.radius,
+    this.onTap,
+  }) : super(key: key);
+  final Widget child;
+
+  final double? height;
+  final double? width;
+  final double? radius;
+  final Color? color;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(radius ?? 24),
+        ),
+        height: height,
+        width: width,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class FGBPTextButton extends StatelessWidget {
+  const FGBPTextButton({
+    Key? key,
+    required this.text,
+    this.height,
+    this.color = AppColorTheme.mainColor,
+    this.onTap,
+  }) : super(key: key);
+
+  final String text;
+  final double? height;
+  final Color? color;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    const textColor = AppTextTheme.regularBlack;
+    return FGBPButton(
+      height: height,
+      onTap: onTap,
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: textColor,
+        ),
+      ),
+    );
+  }
+}
+
+class SmallFGBPButton extends StatelessWidget {
+  const SmallFGBPButton(
+      {Key? key,
+      required this.child,
+      this.height,
+      this.color = AppColorTheme.mainColor,
+      this.onTap})
+      : super(key: key);
+
+  final Widget child;
+  final double? height;
+  final Color? color;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return FGBPButton(
+      onTap: onTap,
+      height: 48,
+      radius: 16,
+      child: child,
+    );
+  }
+}
