ebf312a0d5e0800607f01fe2db54c4cf10fe40f0
Minyong Oh <dhalsdyd@gmail.com>
2022-07-09 10:32:00 +0900

    Add : email login

--- lib/app/firebase/auth.dart
+++ lib/app/firebase/auth.dart
@@ -1,11 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   Future<AuthController> init() async {
     return this;
   }
 
   Future<String> login(String id, String password) async {
     return "";
+  }
+
+  loginWithEmail(String email, String password) async {
+    try {
+      _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
   }
 }
