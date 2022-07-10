230982fb3e800db0d800e4e462895728653a2a8d
Minyong Oh <dhalsdyd@gmail.com>
2022-07-10 13:20:07 +0900

    Add : CRUD method

--- lib/app/firebase/data_document.dart
+++ lib/app/firebase/data_document.dart
@@ -1,14 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class DatabaseManager extends GetxController {
+class DatabaseController extends GetxController {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
-
-  Future add(String name, String id, String password) async {
-    _firebaseFirestore.collection("example").doc(id).set({});
+  Future<DatabaseController> init() async {
+    return this;
   }
 
-  Future delete(String name) async {
-    _firebaseFirestore.collection("example").doc(name).delete();
+  Future<void> addData(String collectionPath, dynamic data) async {
+    _firebaseFirestore.collection(collectionPath).add(data.toJson());
+  }
+
+  Future deleteData(String collectionPath, String id) async {
+    _firebaseFirestore.collection(collectionPath).doc(id).delete();
+  }
+
+  Future updateData(
+      String collectionPath, String id, Map<String, dynamic> update) async {
+    _firebaseFirestore.collection(collectionPath).doc(id).update(update);
+  }
+
+  getStream(String collectionPath) {
+    return _firebaseFirestore.collection(collectionPath).snapshots();
   }
 }
