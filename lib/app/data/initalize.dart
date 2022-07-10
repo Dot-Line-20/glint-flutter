72c75ad917b97504aa5a889c3ddf4e626b74212a
Minyong Oh <dhalsdyd@gmail.com>
2022-07-10 13:29:31 +0900

    Add : D.I in Initalizer

--- lib/app/data/initalize.dart
+++ lib/app/data/initalize.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_getx_boilerplate/app/data/controllers/lifecycle.dart';
+import 'package:firebase_getx_boilerplate/app/firebase/auth.dart';
+import 'package:firebase_getx_boilerplate/app/firebase/data_document.dart';
 import 'package:get/get.dart';
 
 class AppInitalizer {
   Future<void> init() async {
     Get.put<LifeCycleController>(LifeCycleController());
+    await Get.putAsync<DatabaseController>(() => DatabaseController().init());
+    await Get.putAsync<AuthController>(() => AuthController().init());
   }
 }
