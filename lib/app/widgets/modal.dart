3dd56da53e69e2075d08e744270d1e5020957a97
Minyong Oh <dhalsdyd@gmail.com>
2022-07-10 13:24:28 +0900

    Add : dialog style widget

---
+++ lib/app/widgets/modal.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_getx_boilerplate/app/core/color_theme.dart';
+import 'package:flutter/material.dart';
+
+class FGBPDialog extends StatelessWidget {
+  const FGBPDialog(
+      {Key? key, required this.child, this.borderColor = AppColorTheme.white})
+      : super(key: key);
+
+  final Widget child;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      elevation: 2,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColorTheme.white,
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(color: borderColor, width: 3),
+        ),
+        child: Padding(padding: const EdgeInsets.all(24), child: child),
+      ),
+    );
+  }
+}
+
+class CustomFGBPDialog extends StatelessWidget {
+  const CustomFGBPDialog(
+      {Key? key,
+      required this.title,
+      required this.content,
+      this.actions,
+      this.borderColor = AppColorTheme.white})
+      : super(key: key);
+
+  final Widget title;
+  final Widget content;
+  final List<Widget>? actions;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return FGBPDialog(
+      borderColor: borderColor,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            title,
+            const SizedBox(height: 8),
+            content,
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: actions ?? [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
