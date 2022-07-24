ec299c2a6ee1b94ddc05682ca93872729acd12d3
Minyong Oh <dhalsdyd@gmail.com>
2022-07-24 11:18:56 +0900

    FEAT : add firebase firestore CRUD

--- lib/app/firebase/data_document.dart
+++ lib/app/firebase/data_document.dart
@@ -11,16 +11,93 @@ class DatabaseController extends GetxController {
     _firebaseFirestore.collection(collectionPath).add(data.toJson());
   }
 
+  Future<void> addDataFromId(
+      String collectionPath, String id, dynamic data) async {
+    _firebaseFirestore.collection(collectionPath).doc(id).set(data.toJson());
+  }
+
+  Future<void> addDataFromIdWithJson(
+      String collectionPath, String id, dynamic data) async {
+    _firebaseFirestore.collection(collectionPath).doc(id).set(data);
+  }
+
+  Future<void> updateDataFromIdWithJson(
+      String collectionPath, String id, dynamic data) async {
+    _firebaseFirestore.collection(collectionPath).doc(id).update(data);
+  }
+
   Future deleteData(String collectionPath, String id) async {
     _firebaseFirestore.collection(collectionPath).doc(id).delete();
   }
 
-  Future updateData(
-      String collectionPath, String id, Map<String, dynamic> update) async {
-    _firebaseFirestore.collection(collectionPath).doc(id).update(update);
+  Future getData(String collectionPath) async {
+    return _firebaseFirestore.collection(collectionPath).getSavy();
+  }
+
+  Future getSortedData(String collectionPath,
+      {String orderby = "createdAt"}) async {
+    return _firebaseFirestore
+        .collection(collectionPath)
+        .orderBy(orderby)
+        .getSavy();
+  }
+
+  Future getDataFromId(String collectionPath, String id) async {
+    return _firebaseFirestore.collection(collectionPath).doc(id).getSavy();
+  }
+
+  Future getDataFromIdNotCache(String collectionPath, String id) async {
+    return _firebaseFirestore.collection(collectionPath).doc(id).get();
   }
 
   getStream(String collectionPath) {
     return _firebaseFirestore.collection(collectionPath).snapshots();
+  }
+
+  getSortedStream(String collectionPath, {String orderby = "time"}) {
+    return _firebaseFirestore
+        .collection(collectionPath)
+        .orderBy(orderby)
+        .snapshots();
+  }
+
+  Future clearData(String collectionPath) async {
+    final snapshot =
+        await _firebaseFirestore.collection(collectionPath).getSavy();
+    for (var doc in snapshot.docs) {
+      _firebaseFirestore.collection(collectionPath).doc(doc.id).delete();
+    }
+  }
+
+  Future clearSetData(String collectionPath, dynamic data) async {
+    final snapshot =
+        await _firebaseFirestore.collection(collectionPath).getSavy();
+    for (var doc in snapshot.docs) {
+      _firebaseFirestore.collection(collectionPath).doc(doc.id).update(data);
+    }
+  }
+}
+
+extension FirestoreDocumentExtension on DocumentReference {
+  Future<DocumentSnapshot> getSavy() async {
+    try {
+      DocumentSnapshot ds = await get(const GetOptions(source: Source.cache));
+      if (ds == null) return get(const GetOptions(source: Source.server));
+      return ds;
+    } catch (_) {
+      return get(const GetOptions(source: Source.server));
+    }
+  }
+}
+
+extension FirestoreQueryExtension on Query {
+  Future<QuerySnapshot> getSavy() async {
+    try {
+      QuerySnapshot qs = await get(const GetOptions(source: Source.cache));
+      if (qs.docs.isEmpty) return get(const GetOptions(source: Source.server));
+      return qs;
+    } catch (_) {
+      return get(const GetOptions(source: Source.server));
+    }
   }
 }
