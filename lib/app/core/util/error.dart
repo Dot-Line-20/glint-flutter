441a62c03881f046bb524b844de9251679299930
Minyong Oh <dhalsdyd@gmail.com>
2022-08-18 19:53:36 +0900

    FEAT : add uitls

---
+++ lib/app/core/util/error.dart
@@ -0,0 +1,4 @@
+class OnboardingException implements Exception {
+  final String message;
+  OnboardingException(this.message);
+}
--- lib/app/widgets/snackbar.dart
+++ lib/app/widgets/snackbar.dart
@@ -5,6 +5,26 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FGBPSnackBar {
+  static void open(String title,
+      {Color backgroundColor = AppColorTheme.black,
+      Color textColor = AppColorTheme.black}) {
+    Get.rawSnackbar(
+      titleText: Text(title,
+          style: TextStyle(
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+              height: 1,
+              color: textColor)),
+      messageText: Container(),
+      backgroundColor: backgroundColor,
+      borderRadius: 12,
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      animationDuration: const Duration(milliseconds: 800),
+    );
+  }
+
   static void openOne(String title, String one) {
     Get.rawSnackbar(
       onTap: (_) {
