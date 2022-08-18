510cc3b3254e7fb7de3ec48f4ff72085501eeb2a
Minyong Oh <dhalsdyd@gmail.com>
2022-08-18 19:40:32 +0900

    FEAT : refacotring theme

--- lib/app/pages/home/page.dart
+++ lib/app/pages/home/page.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_getx_boilerplate/app/core/theme/text_theme.dart';
+import 'package:firebase_getx_boilerplate/app/widgets/button.dart';
+import 'package:firebase_getx_boilerplate/app/widgets/textfield.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,12 +8,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController textEditingController = TextEditingController();
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: const [
-          Text("Hello World!!!"),
-        ],
+          child: Padding(
+        padding: const EdgeInsets.all(44),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Hello World!!!", style: AppTextTheme.bold20),
+            const SizedBox(height: 8),
+            FGBPTextField(textController: textEditingController),
+            const SizedBox(height: 8),
+            FGBPTextButton(text: "Hello", radius: 10),
+          ],
+        ),
       )),
     );
   }
