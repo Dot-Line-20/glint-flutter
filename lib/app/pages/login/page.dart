c078fc1429965f4e31585e45027d29eb41a78936
Minyong Oh <dhalsdyd@gmail.com>
2022-10-05 20:56:09 +0900

    Make Scrollable

--- lib/app/pages/login/page.dart
+++ lib/app/pages/login/page.dart
@@ -20,90 +20,92 @@ class LoginPage extends GetView<LoginPageController> {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("로그인", style: AppTextTheme.boldHGray1_24),
-            const SizedBox(height: 8),
-            const Text("홍보용 문구", style: AppTextTheme.boldGray2_14),
-            const SizedBox(height: 70),
-            Form(
-              key: controller.formKey,
-              child: FocusScope(
-                  node: controller.formFocusScopeNode,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text("아이디", style: AppTextTheme.boldGray3_14),
-                      FGBPTextFormField(
-                        enableInteractiveSelection: false,
-                        controller: controller.idTextController,
-                      ),
-                      const SizedBox(height: 50),
-                      const Text("비밀번호", style: AppTextTheme.boldGray3_14),
-                      FGBPTextFormField(
-                        isPassword: true,
-                        enableInteractiveSelection: false,
-                        controller: controller.passwordTextController,
-                      ),
-                    ],
-                  )),
-            ),
-            const SizedBox(height: 50),
-            controller.obx(
-              (_) => Obx(
-                () => FGBPKeyboardReactiveButton(
-                  disabled: !controller.inputValidity,
-                  onTap: controller.inputValidity ? controller.login : null,
-                  child: const Text(
-                    "로그인하기",
-                    style: AppTextTheme.boldWhite_18,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("로그인", style: AppTextTheme.boldHGray1_24),
+              const SizedBox(height: 8),
+              const Text("홍보용 문구", style: AppTextTheme.boldGray2_14),
+              const SizedBox(height: 70),
+              Form(
+                key: controller.formKey,
+                child: FocusScope(
+                    node: controller.formFocusScopeNode,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("아이디", style: AppTextTheme.boldGray3_14),
+                        FGBPTextFormField(
+                          enableInteractiveSelection: false,
+                          controller: controller.idTextController,
+                        ),
+                        const SizedBox(height: 50),
+                        const Text("비밀번호", style: AppTextTheme.boldGray3_14),
+                        FGBPTextFormField(
+                          isPassword: true,
+                          enableInteractiveSelection: false,
+                          controller: controller.passwordTextController,
+                        ),
+                      ],
+                    )),
+              ),
+              const SizedBox(height: 50),
+              controller.obx(
+                (_) => Obx(
+                  () => FGBPKeyboardReactiveButton(
+                    disabled: !controller.inputValidity,
+                    onTap: controller.inputValidity ? controller.login : null,
+                    child: const Text(
+                      "로그인하기",
+                      style: AppTextTheme.boldWhite_18,
+                    ),
+                  ),
+                ),
+                onLoading: const FGBPKeyboardReactiveButton(
+                  disabled: false,
+                  child: SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: CircularProgressIndicator(
+                      color: AppColorTheme.white,
+                      strokeWidth: 2,
+                    ),
                   ),
                 ),
               ),
-              onLoading: const FGBPKeyboardReactiveButton(
-                disabled: false,
-                child: SizedBox(
-                  height: 20,
-                  width: 20,
-                  child: CircularProgressIndicator(
-                    color: AppColorTheme.white,
-                    strokeWidth: 2,
-                  ),
-                ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LoginHelper(onTap: () {}, content: "아이디 찾기"),
+                  const SizedBox(width: 30),
+                  LoginHelper(onTap: () {}, content: "비밀번호 찾기"),
+                  const SizedBox(width: 30),
+                  // LoginHelper(
+                  //     onTap: controller.moveToSchedulePage, content: "일정 보기"),
+                  // const SizedBox(width: 30),
+                  LoginHelper(
+                      onTap: controller.moveToRegisterPage, content: "회원가입하기"),
+                ],
               ),
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                LoginHelper(onTap: () {}, content: "아이디 찾기"),
-                const SizedBox(width: 30),
-                LoginHelper(onTap: () {}, content: "비밀번호 찾기"),
-                const SizedBox(width: 30),
-                // LoginHelper(
-                //     onTap: controller.moveToSchedulePage, content: "일정 보기"),
-                // const SizedBox(width: 30),
-                LoginHelper(
-                    onTap: controller.moveToRegisterPage, content: "회원가입하기"),
-              ],
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SocialLoginButton(
-                    onTap: () {}, imgUrl: "assets/images/facebook.png"),
-                const SizedBox(width: 20),
-                SocialLoginButton(
-                    onTap: () {}, imgUrl: "assets/images/kakao.png"),
-                const SizedBox(width: 20),
-                SocialLoginButton(
-                    onTap: () {}, imgUrl: "assets/images/google.png"),
-              ],
-            ),
-          ],
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialLoginButton(
+                      onTap: () {}, imgUrl: "assets/images/facebook.png"),
+                  const SizedBox(width: 20),
+                  SocialLoginButton(
+                      onTap: () {}, imgUrl: "assets/images/kakao.png"),
+                  const SizedBox(width: 20),
+                  SocialLoginButton(
+                      onTap: () {}, imgUrl: "assets/images/google.png"),
+                ],
+              ),
+            ],
+          ),
         ),
       )),
     );
