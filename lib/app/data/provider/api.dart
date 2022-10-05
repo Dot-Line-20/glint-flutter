ddfecded62033b9783f2c6f561e629e16cbe59b9
Minyong Oh <dhalsdyd@gmail.com>
2022-10-05 20:29:49 +0900

    Update login processing

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:get/instance_manager.dart';
 import 'package:glint/app/data/provider/api_interface.dart';
+import 'package:glint/app/data/service/auth/service.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class FGBPInterceptor extends Interceptor {
   final Dio _dioInstance;
@@ -16,6 +19,53 @@ class FGBPInterceptor extends Interceptor {
   // void onError(DioError err, ErrorInterceptorHandler handler) {
   //   super.onError(err, handler);
   // }
+}
+
+class JWTInterceptor extends Interceptor {
+  final Dio _dioInstance;
+
+  // Dependency Injection
+  JWTInterceptor(this._dioInstance);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (options.path == '/auth/refresh') {
+      return handler.next(options);
+    }
+
+    AuthService authService = Get.find<AuthService>();
+
+    if (authService.isAuthenticated) {
+      options.headers['Authorization'] = 'Bearer ${authService.accessToken}';
+    }
+
+    return handler.next(options);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    AuthService authService = Get.find<AuthService>();
+    //refresh api가 401시 무한 루프 방지
+    if (err.response?.requestOptions.path == '/auth/refresh') {
+      return handler.next(err);
+    }
+
+    if (err.response?.statusCode == 401 &&
+        authService.accessToken != null &&
+        JwtDecoder.isExpired(authService.accessToken!)) {
+      try {
+        await authService.refreshAcessToken();
+
+        //api 호출을 다시 시도함
+        final Response response = await _dioInstance.fetch(err.requestOptions);
+        return handler.resolve(response);
+      } catch (e) {
+        //refresh 실패 시 401을 그대로 반환
+        return handler.next(err);
+      }
+    }
+    return handler.next(err);
+  }
 }
 
 class LogInterceptor extends Interceptor {
--- lib/app/data/service/auth/service.dart
+++ lib/app/data/service/auth/service.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:glint/app/data/service/auth/repository.dart';
 
 class AuthService extends GetxService {
   final AuthRepository repository;
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
   final Rx<String?> _accessToken = Rx(null);
   final Rx<String?> _refreshToken = Rx(null);
 
@@ -16,6 +19,22 @@ class AuthService extends GetxService {
   String? get refreshToken => _refreshToken.value;
 
   AuthService(this.repository);
+
+  Future<AuthService> init() async {
+    _accessToken.value = await _storage.read(key: 'accessToken');
+    _refreshToken.value = await _storage.read(key: 'refreshToken');
+    return this;
+  }
+
+  Future<void> _setAccessToken(String token) async {
+    await _storage.write(key: 'accessToken', value: token);
+    _accessToken.value = token;
+  }
+
+  Future<void> _setRefreshToken(String token) async {
+    await _storage.write(key: 'refreshToken', value: token);
+    _refreshToken.value = token;
+  }
 
   Future<String> registerUser(
       String email, String password, String name, String birth) async {
@@ -38,12 +57,21 @@ class AuthService extends GetxService {
       Map loginResult = await repository.login(email, password);
       print(loginResult);
       if (loginResult["status"] == "success") {
-        _accessToken.value = loginResult["data"]["accessToken"];
-        _refreshToken.value = loginResult["data"]["refreshToken"];
+        _setAccessToken(loginResult["data"]["accessToken"]);
+        _setRefreshToken(loginResult["data"]["refreshToken"]);
       }
     } on DioError catch (e) {
       print(e.response!.statusCode.toString());
       rethrow;
     }
+  }
+
+  Future<void> refreshAcessToken() async {}
+
+  Future<void> logout() async {
+    _accessToken.value = null;
+    _refreshToken.value = null;
+    await _storage.delete(key: 'accessToken');
+    await _storage.delete(key: 'refreshToken');
   }
 }
