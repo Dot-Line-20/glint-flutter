130248a223595a298de8f8290af71b4ea250a17c
Minyong Oh <dhalsdyd@gmail.com>
2022-10-06 21:41:10 +0900

    Make non_constant_identifier_names

--- lib/app/data/models/message.dart
+++ lib/app/data/models/message.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, non_constant_identifier_names
 
 import 'package:json_annotation/json_annotation.dart';
 part 'message.g.dart';
