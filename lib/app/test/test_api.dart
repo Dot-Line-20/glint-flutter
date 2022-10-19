15122bc8ca667ecf583d803a6098deeecd8c1f21
Minyong Oh <dhalsdyd@gmail.com>
2022-10-20 08:10:54 +0900

    Add TestApi

---
+++ lib/app/test/test_api.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:glint/app/test/test_model.dart';
+
+class TestApi extends GetConnect {
+  Future<List<TestPost>> fetchPosts(int page, int limit) async {
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    Response response = await get("$url?_page=$page&_limit=$limit");
+    print(response.body);
+    return response.body
+        .map<TestPost>((post) => TestPost.fromJson(post))
+        .toList();
+  }
+}
+
+class ImageApi extends GetConnect {
+  @override
+  final baseUrl = "https://source.unsplash.com/random/";
+
+  Future<String> getImage(int count) async {
+    return "$baseUrl/$count";
+  }
+}
