e70d00d284748433ad84cf28b05bebe1f475df8d
Minyong Oh <dhalsdyd@gmail.com>
2022-11-14 11:43:53 +0900

    Fix Svg warning

--- lib/app/pages/schedule/widget/schedule.dart
+++ lib/app/pages/schedule/widget/schedule.dart
@@ -7,6 +7,7 @@ import 'package:glint/app/data/module/schedule/schedule.dart';
 import 'package:glint/app/pages/schedule/widget/detail.dart';
 
 import 'package:glint/app/core/util/extension.dart';
+import 'package:glint/app/widgets/empty.dart';
 
 class ScheduleList extends StatelessWidget {
   final List<Schedule> scheduleList;
@@ -15,7 +16,12 @@ class ScheduleList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return scheduleList.isNotEmpty ? _exist() : _notExist();
+    return scheduleList.isNotEmpty
+        ? _exist()
+        : const Empty(
+            title: "일정 없음",
+            description: "",
+          );
   }
 
   Widget _exist() {
@@ -106,32 +112,6 @@ class ScheduleList extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _notExist() {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SvgPicture.asset("assets/images/none.svg", width: 150, height: 150),
-          const SizedBox(height: 16),
-          const Text("아직 일정이 없네요", style: AppTextTheme.T5),
-          const SizedBox(height: 16),
-          Text.rich(
-            TextSpan(
-              style: AppTextTheme.T7.copyWith(color: AppColorTheme.Gray2),
-              children: const [
-                TextSpan(text: "추가하기 버튼을 통해\n"),
-                TextSpan(text: "빠르게 일정을 추가할 수 있습니다"),
-              ],
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
       ),
     );
   }
